bb8008c19d0e8e4f407e064a009910e02baf22d5
Quang Tran <quangtqag@gmail.com>
2017-05-31 16:52:36 +0700

    - update

--- BrokenImage/BrokenImage/AppDelegate.h
+++ BrokenImage/BrokenImage/AppDelegate.h
@@ -5,6 +5,10 @@
 //  Created by Quang Tran on 5/28/17.
 //  Copyright © 2017 Quang Tran. All rights reserved.
 //
+// CREDIT:
+// Inspired from design of Divan Raj
+// https://dribbble.com/shots/3467119-Swipe-Delete-interaction
+//
 
 #import <UIKit/UIKit.h>
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BrokenViewEffect
 A demo for effect about view is broken into pieces
+
+Inspired from design of [Divan Raj](https://dribbble.com/shots/3467119-Swipe-Delete-interaction)
+
+![Demo image]()
